49ff9ba39036962e19e706f528f2f7b624097ae6
Tomus_kd35 <Tomus_kd35@student.itstep.org>
2024-05-24 19:42:40 +0300

    Add project files.

---
+++ Task4.cs
@@ -0,0 +1,36 @@
+﻿
+namespace ConsoleApp26
+{
+    abstract class Worker
+    {
+        public abstract void Print();
+    }
+    class President : Worker
+    {
+        public override void Print()
+        {
+            Console.WriteLine("This is the President of the company.");
+        }
+    }
+    class Security : Worker
+    {
+        public override void Print()
+        {
+            Console.WriteLine("This is the Security personnel.");
+        }
+    }
+    class Manager : Worker
+    {
+        public override void Print()
+        {
+            Console.WriteLine("This is the Manager of the department.");
+        }
+    }
+    class Engineer : Worker
+    {
+        public override void Print()
+        {
+            Console.WriteLine("This is an Engineer working on projects.");
+        }
+    }
+}
